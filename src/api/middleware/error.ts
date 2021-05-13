6dc518f9871609989fe00fd7a2b8ef01e4d605b9
Toluwani <toluwhani@gmail.com>
2021-05-13 13:46:48 +0300

    cleaned up error files for readability

--- src/api/middleware/error.ts
+++ src/api/middleware/error.ts
@@ -1,10 +1,12 @@
 import HttpStatus from 'http-status-codes'
 import { Request, Response } from 'express'
+
 export interface IError {
   status?: number
   code?: number
   message?: string
 }
+
 export const notFoundErrorHandler = (req: Request, res: Response): void => {
   res.status(HttpStatus.NOT_FOUND || 500).json({
     message: HttpStatus.getStatusText(HttpStatus.NOT_FOUND),
