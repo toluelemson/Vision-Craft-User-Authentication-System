a22b117fa9f0775c58fe9f071127c970abf5d417
toluelemson <toluwanielemosho@gmail.com>
2021-05-13 11:13:40 +0300

    modified database configuration

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,23 +1,43 @@
-interface Config {
-  PORT: number
-  NODE_ENV: string
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const MYSQL_HOST = process.env.MYSQL_HOST || 'localhost'
+const MYSQL_PORT = process.env.MYSQL_PORT || '3306'
+const MYSQL_DATABASE = process.env.MYSQL_DATABASE || 'uas'
+const MYSQL_USER = process.env.MYSQL_USER || 'root'
+const MYSQL_PASS = process.env.MYSQL_PASSWORD || ''
+
+const MYSQL = {
+  host: MYSQL_HOST,
+  port: MYSQL_PORT,
+  database: MYSQL_DATABASE,
+  user: MYSQL_USER,
+  pass: MYSQL_PASS,
 }
 
-export const config: Config = {
-  PORT: 8080,
-  NODE_ENV: 'development',
-}
+const SERVER_HOSTNAME = process.env.SERVER_HOSTNAME || 'localhost'
+const SERVER_PORT = process.env.SERVER_PORT || 8080
+const SERVER_TOKEN_EXPIRETIME = process.env.SERVER_TOKEN_EXPIRETIME || 3600
+const SERVER_TOKEN_ISSUER = process.env.SERVER_TOKEN_ISSUER || 'issuer'
+const SERVER_TOKEN_SECRET = process.env.SERVER_TOKEN_SECRET || 'encryptedsecret'
 
-export const dbConfig = {
-  HOST: '',
-  USER: '',
-  PASSWORD: '',
-  DB: '',
-  dialect: '',
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
+const SERVER = {
+  HOSTNAME: SERVER_HOSTNAME,
+  PORT: SERVER_PORT,
+  TOKEN: {
+    expireTime: SERVER_TOKEN_EXPIRETIME,
+    issuer: SERVER_TOKEN_ISSUER,
+    secret: SERVER_TOKEN_SECRET,
   },
 }
+
+const NODE_ENV = process.env.NODE_ENV || 'development'
+
+const config = {
+  MYSQL,
+  SERVER,
+  NODE_ENV,
+}
+
+export default config
