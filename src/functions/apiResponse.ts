68b006933c7f51546ba3ae5fa7a22da6a03f7f39
Toluwani <toluwhani@gmail.com>
2021-05-13 13:56:37 +0300

    created funtion that handles api responses

---
+++ src/functions/apiResponse.ts
@@ -0,0 +1,32 @@
+import { Response } from 'express'
+import httpStatusCodes from 'http-status-codes'
+import { userData } from '@src/api/components/auth/user.interface'
+
+class ApiResponse extends Error {
+  static result = (res: Response, data: userData, status = 200, token?: string | null): void => {
+    res.status(status)
+    if (token) {
+      res.json({
+        data,
+        token,
+        success: true,
+      })
+    }
+    res.json({
+      data,
+      success: true,
+    })
+  }
+
+  static error = (res: Response, status = 400, error: string = httpStatusCodes.getStatusText(status)): void => {
+    console.log(error)
+    res.status(status).json({
+      error: {
+        message: error,
+      },
+      success: false,
+    })
+  }
+}
+
+export default ApiResponse
