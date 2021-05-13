8f8c4b8d59d943c8cae479e0d8dd3fa21b9308f1
Toluwani <toluwhani@gmail.com>
2021-05-13 14:12:51 +0300

    created a helper function that signs JWT token

--- src/functions/encryptionUtils.ts
+++ src/functions/encryptionUtils.ts
@@ -12,6 +12,36 @@ const generateHash = async (password: string, saltRounds: number): Promise<strin
     })
   })
 
+// eslint-disable-next-line no-unused-vars
+const signJWT = (user: string, callback: (error: Error | null, token: string | null) => void): void => {
+  const timeSinceEpoch = new Date().getTime()
+  const expirationTime = timeSinceEpoch + Number(config.SERVER.TOKEN.expireTime) * 100000
+  const expirationTimeInSeconds = Math.floor(expirationTime / 1000)
+
+  try {
+    jwt.sign(
+      {
+        username: user,
+      },
+      config.SERVER.TOKEN.secret,
+      {
+        issuer: config.SERVER.TOKEN.issuer,
+        algorithm: 'HS256',
+        expiresIn: expirationTimeInSeconds,
+      },
+      (error, token) => {
+        if (error) {
+          callback(error, null)
+        } else if (token) {
+          callback(null, token)
+        }
+      }
+    )
+  } catch (error) {
+    callback(error, null)
+  }
+}
+
 const verifyHash = async (password: string, hash: string): Promise<boolean> => {
   return new Promise((resolve, reject) => {
     bcryptjs.compare(password, hash, (err, result) => {
@@ -21,3 +51,4 @@ const verifyHash = async (password: string, hash: string): Promise<boolean> => {
   })
 }
 
+export { generateHash, signJWT, verifyHash }
