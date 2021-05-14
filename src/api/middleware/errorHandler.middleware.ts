aece3ff8f9ce975789255a756abd545849e534a4
Toluwani <toluwhani@gmail.com>
2021-05-14 13:14:52 +0300

    returned error-handler functions.

--- src/api/middleware/errorHandler.middleware.ts
+++ src/api/middleware/errorHandler.middleware.ts
@@ -17,6 +17,23 @@ interface IJoiError extends IError {
   details?: Array<IJoiErrorDetail>
 }
 
+export const notFoundErrorHandler = (req: Request, res: Response): void => {
+  res.status(HttpStatus.NOT_FOUND || 500).json({
+    message: HttpStatus.getStatusText(HttpStatus.NOT_FOUND),
+    code: HttpStatus.NOT_FOUND,
+  })
+}
+
+export function internalErrorHandler(err: IError, req: Request, res: Response) {
+  res.status(err.status || HttpStatus.INTERNAL_SERVER_ERROR).json({
+    success: false,
+    error: {
+      code: err.code || HttpStatus.INTERNAL_SERVER_ERROR,
+      message: err.message || HttpStatus.getStatusText(HttpStatus.INTERNAL_SERVER_ERROR),
+    },
+  })
+}
+
 /**
  * Joi error handler middleware
  */
