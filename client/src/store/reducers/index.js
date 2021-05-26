1a6b3f01a29a9add9ad646acdb05475aad90cb5b
Toluwani <toluwhani@gmail.com>
2021-05-26 20:46:09 +0300

    delete files

--- client/src/store/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import { userLoginReducer, userRegisterReducer, userDetailsReducer } from './user.reducer';
-
-export default combineReducers({
-  userLogin: userLoginReducer,
-  userRegister: userRegisterReducer,
-  userDetails: userDetailsReducer,
-
-});
